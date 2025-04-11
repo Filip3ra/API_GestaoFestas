59272f383a29a9c0ebf48dc366597ae36d1cc10c
Filip3ra <filipijardimrm@gmail.com>
2025-04-11 11:14:53 -0300

    feat: ocultar e mostrar eventos listados

--- EventManagement/obj/Debug/net8.0/EventManagement.AssemblyInfo.cs
+++ EventManagement/obj/Debug/net8.0/EventManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a709a6e9757ca2082259af996753da5af3c029b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7783a2c2f9e86e7276990374503e9311808f92fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
